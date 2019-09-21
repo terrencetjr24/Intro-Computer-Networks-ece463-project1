e1ae688a8e9e7582038a8185e3fb046d7acbc59f
terrencetjr24 <randall7@purdue.edu>
2019-09-21 16:10:14 -0400

    first commit

---
+++ part4/part4/multi_service_server.randall7.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  part4
+//
+//  Created by terrence on 9/21/19.
+//  Copyright © 2019 terrence. All rights reserved.
+//
+
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <strings.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
