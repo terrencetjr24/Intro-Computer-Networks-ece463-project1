42fed97575157d2f6d4de2f55f6a35bebb768d79
terrencetjr24 <randall7@purdue.edu>
2019-09-24 21:10:23 -0400

    ghgh

--- part4/part4/multi_service_server.randall7.c
+++ part4/part4/multi_service_server.randall7.c
@@ -127,7 +127,7 @@ int main(int argc, const char * argv[]) {
             recievedNum = (buf2 + 14);
             //uint32_t recievedNum = atoi(*recNum);
             //recvfrom(pingfd, (void*) &recievedNum, sizeof(uint32_t), 0, ( struct sockaddr *) &pingClientaddr, &pingClientlen);
-            puts(*recievedNum);
+            puts(recievedNum);
             pingClientaddr.sin_addr.s_addr = inet_addr(buf);
             
             if (getnameinfo((struct sockaddr *) &pingClientaddr, pingClientlen, hostname, sizeof(hostname), NULL, 0, NI_NAMEREQD)) {
