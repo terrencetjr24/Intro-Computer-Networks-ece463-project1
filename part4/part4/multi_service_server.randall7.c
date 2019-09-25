572131e3ca7aca903155d020817386ba74c66446
terrencetjr24 <randall7@purdue.edu>
2019-09-25 00:35:38 -0400

    hgh

--- part4/part4/multi_service_server.randall7.c
+++ part4/part4/multi_service_server.randall7.c
@@ -125,10 +125,10 @@ int main(int argc, const char * argv[]) {
             
             n = recvfrom(pingfd, (char*)buf, MAXLINE, 0, (struct sockaddr *) &pingClientaddr, &pingClientlen);
             printf("This is how many bytes were read: %d\n", n);
-            buf[n] = '\0';
+            //buf[n] = '\0';
             printf("This is what was recieved\n");
             puts(buf);
-            buf[n] = 0;
+            //buf[n] = 0;
 
             uint8_t byte1 = buf[n-4];
             uint8_t byte2 = buf[n-3];
