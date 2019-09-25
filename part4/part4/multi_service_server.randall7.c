9065182469b99ad47b83debab2e8ed99bb9340ef
terrencetjr24 <randall7@purdue.edu>
2019-09-25 02:01:56 -0400

    gjgj

--- part4/part4/multi_service_server.randall7.c
+++ part4/part4/multi_service_server.randall7.c
@@ -151,7 +151,7 @@ int main(int argc, const char * argv[]) {
             writeBuf[i] = byte4;
         
             //I have the hostname, but I also want to send some number +1 as well
-            n = sendto(pingfd, (char *)&writeBuf, strlen(hostname) + (sizeof(uint8_t) *4), MSG_CONFIRM, (const struct sockaddr *)&pingClientaddr, pingClientlen);
+            n = sendto(pingfd, (char *)&writeBuf, strlen(hostname) + (sizeof(uint8_t) *4)+1, MSG_CONFIRM, (const struct sockaddr *)&pingClientaddr, pingClientlen);
         
             //n = sendto(pingfd, (char *)&hostname, strlen(hostname), 0, (const struct sockaddr *) &pingClientaddr, pingClientlen);
             printf("The number of bytes sent: %d\n\n", n);
